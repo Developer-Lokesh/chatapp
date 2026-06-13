9abec2d8c0017ea188e8f2e8966c04149d22a27d
Lokesh Kashyap <9027410147lk@gmail.com>
2026-06-13 15:08:41 +0530

    fix: fix minor bugs

--- server/app.js
+++ server/app.js
@@ -34,12 +34,13 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: " http://localhost:5173", //|| "https://ghostchat-qzok.onrender.com" ,
+    origin:   "https://ghostchat-qzok.onrender.com" ,
+    //" http://localhost:5173", 
     credentials: true,
   },
 });
 
-app.use(cors({ origin: " http://localhost:5173", //|| "https://ghostchat-qzok.onrender.com" ,
+app.use(cors({ origin:  "https://ghostchat-qzok.onrender.com" ,  //http://localhost:5173",
  credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
